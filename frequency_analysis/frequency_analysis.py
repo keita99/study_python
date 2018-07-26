a331d73fdb4a93459055b8432f72afbd6ddc01d4
Keita Nakagami <keita00@gmail.com>
2018-07-26 23:05:21 +0900

    string lower and replace space

--- frequency_analysis/frequency_analysis.py
+++ frequency_analysis/frequency_analysis.py
@@ -1,7 +1,15 @@
+#
+# 頻度分析
+#
+
 string = str(input(" Please input string : "))
+string = string.lower() #入力された文字を小文字化
+string = string.replace(' ','') #空白を削除
+
+print (string)
 
 check_char = 'a'
 while check_char <= 'z': 
     c = string.count(check_char)
-    print(check_char, " : ", c)
+    print(check_char, ":", c)
     check_char = chr(ord(check_char) + 1)
